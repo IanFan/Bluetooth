c971329189f42b001cce7631d10bf03e5edddb3c
IanFan <ianfan0704@gmail.com>
2013-12-22 15:09:26 +0800

    Add "Bluetooth" new project. Add .gitignore.

---
+++ BluetoothTests/BluetoothTests.m
@@ -0,0 +1,34 @@
+//
+//  BluetoothTests.m
+//  BluetoothTests
+//
+//  Created by Ian Fan on 22/12/2013.
+//  Copyright (c) 2013 Ian Fan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BluetoothTests : XCTestCase
+
+@end
+
+@implementation BluetoothTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
